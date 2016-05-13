34c6d7ae9582cbe5dc6d5448bf20da89a5b7ed85
buildsrv <dev@bluechilli.com>
2016-05-13 12:05:29 +1000

    build

--- Source/deployscript.bat
+++ Source/deployscript.bat
@@ -6,6 +6,6 @@ IF [%COMMITCOMMENT%] EQU [""] goto :nocomment
 
 @ECHO ON
 
-nuget pack "Xipton.Razor.nuspec"
+nuget pack "Source\Xipton.Razor.nuspec"
 
-nuget push "*.nupkg" 1660902d-ef34-4ce3-8217-bef69d90ba6b -s https://www.myget.org/F/bluechilli-new/api/v2/package
+nuget push "Source\*.nupkg" 1660902d-ef34-4ce3-8217-bef69d90ba6b -s https://www.myget.org/F/bluechilli-new/api/v2/package
